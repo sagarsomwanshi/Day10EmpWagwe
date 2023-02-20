d0027ade54b21a208fa8d92fd56bd12fcfc65e11
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-02-20 19:36:17 +0530

    Added : Stores daily wage along with total wage

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         WageCalculation wage = new WageCalculation();
         wage.companyDetails();
         System.out.println(wage.EmpWageBuilder);
+        System.out.println("Daily Wage of "+ wage.EmpWageBuilder.get(0).getCompanyName()+" company : "+Arrays.toString(wage.wageStore));
         wage.addCompany();
     }
 }
--- src/WageCalculation.java
+++ src/WageCalculation.java
@@ -1,8 +1,11 @@
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class WageCalculation implements Interface {
     ArrayList<Company> EmpWageBuilder = new ArrayList<>();
+    int [] wageStore = new int[30];
 
     static int workHour,workDay;
 
@@ -16,8 +19,16 @@ public class WageCalculation implements Interface {
         int maxHours = sc.nextInt();
         System.out.println("Enter maximum working days per month");
         int maxWorkDays = sc.nextInt();
+        int j = 1;
+        workHour =0;
+        workDay=0;
         while(workDay < maxWorkDays && workHour < maxHours){
+            int i = workHour;
             dailyWork();
+            int wage = (workHour-i)*20;
+            System.out.println("Day "+ j + " : "+wage);
+            wageStore[j] = wage;
+            j= j+1;
         }
         int totalWage = workHour*hourWage;
         Company comp = new Company(companyName,totalWage,workDay);
@@ -35,20 +46,24 @@ public class WageCalculation implements Interface {
             case 0 :
                 workHour += 8;
                 workDay++;
-                break;
+
+            break;
             case 1:
                 workHour += 4;
                 workDay++;
                 break;
         }
     }
+    int check1 = 1;
     public void addCompany() {
-        System.out.println("To calculate wage for another company press 1\n press 2 to close the employee wage calculation program");
+        System.out.println("To calculate wage for another company Enter 1\nTo close the employee wage calculation program Enter 2");
         Scanner sc = new Scanner(System.in);
         int check = sc.nextInt();
         if (check == 1) {
             companyDetails();
             System.out.println(EmpWageBuilder);
+            System.out.println("Daily Wage of "+EmpWageBuilder.get(check1).getCompanyName()+" company : "+ Arrays.toString(wageStore));
+            check1 = check1+1;
             addCompany();
         } else {
             System.out.println("Company wage calculation closed");
