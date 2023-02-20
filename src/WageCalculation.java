64922ccff8d475c5c78c70e5411e94e91e0ff491
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-02-20 20:13:33 +0530

    Added : Gives total wage of any company

--- src/WageCalculation.java
+++ src/WageCalculation.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 
 public class WageCalculation implements Interface {
     ArrayList<Company> EmpWageBuilder = new ArrayList<>();
-    int [] wageStore = new int[30];
+    int[] wageStore = new int[30];
 
-    static int workHour,workDay;
+    static int workHour, workDay;
 
-    public void companyDetails(){
+    public void companyDetails() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Company Name");
         String companyName = sc.nextLine();
@@ -20,53 +20,81 @@ public class WageCalculation implements Interface {
         System.out.println("Enter maximum working days per month");
         int maxWorkDays = sc.nextInt();
         int j = 1;
-        workHour =0;
-        workDay=0;
-        while(workDay < maxWorkDays && workHour < maxHours){
+        workHour = 0;
+        workDay = 0;
+        while (workDay < maxWorkDays && workHour < maxHours) {
             int i = workHour;
             dailyWork();
-            int wage = (workHour-i)*20;
-            System.out.println("Day "+ j + " : "+wage);
+            int wage = (workHour - i) * 20;
+            System.out.println("Day " + j + " : " + wage);
             wageStore[j] = wage;
-            j= j+1;
+            j = j + 1;
         }
-        int totalWage = workHour*hourWage;
-        Company comp = new Company(companyName,totalWage,workDay);
+        int totalWage = workHour * hourWage;
+        Company comp = new Company(companyName, totalWage, workDay);
         EmpWageBuilder.add(comp);
 
     }
-    public int attendance(){
+
+    public int attendance() {
         int attendance = (int) ((Math.random() * 10) % 3);
         return attendance;
     }
-    public static void dailyWork(){
+
+    public static void dailyWork() {
         WageCalculation c = new WageCalculation();
 
         switch (c.attendance()) {
-            case 0 :
+            case 0:
                 workHour += 8;
                 workDay++;
 
-            break;
+                break;
             case 1:
                 workHour += 4;
                 workDay++;
                 break;
         }
     }
+
     int check1 = 1;
+
     public void addCompany() {
-        System.out.println("To calculate wage for another company Enter 1\nTo close the employee wage calculation program Enter 2");
+        System.out.println("To calculate wage for another company Enter 1\nTo get total wage of any company Enter 2\nTo close the program Enter 3");
         Scanner sc = new Scanner(System.in);
         int check = sc.nextInt();
         if (check == 1) {
             companyDetails();
             System.out.println(EmpWageBuilder);
-            System.out.println("Daily Wage of "+EmpWageBuilder.get(check1).getCompanyName()+" company : "+ Arrays.toString(wageStore));
-            check1 = check1+1;
+            System.out.println("Daily Wage of " + EmpWageBuilder.get(check1).getCompanyName() + " company : " + Arrays.toString(wageStore));
+            check1 = check1 + 1;
             addCompany();
+        } else if (check == 2) {
+            getWage();
+
         } else {
             System.out.println("Company wage calculation closed");
         }
+
+    }
+
+    public void getWage() {
+        System.out.println("Enter Name of company to Get total Wage");
+        int check3 = 0;
+        Scanner sc = new Scanner(System.in);
+        String check2 = sc.nextLine();
+        for (int i = 0; i < EmpWageBuilder.size(); i++) {
+            String Check1 = EmpWageBuilder.get(i).getCompanyName();
+            if (Check1.equalsIgnoreCase(check2)) {
+                System.out.println("Total Wage of " + EmpWageBuilder.get(i).getCompanyName() + " company : " + EmpWageBuilder.get(i).getTotalWage());
+                check3 =1;
+            }
+        }
+        if(check3 != 1){
+            System.out.println("Name not found");
+        }
+        addCompany();
     }
 }
+
+
